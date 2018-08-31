d02ef589c31cfdc5a595a72bfff965724e557e61
Meu Git Jason <freithasadriano@gmail.com>
2018-08-31 17:41:41 -0300

    PRIMEIRO PROGRAMA DE CALCULADORA EM PYTHON

---
+++ calculo.py
@@ -0,0 +1,7 @@
+def soma(x):
+	return(x)
+def result(z):
+     z = 4 * 4
+print('Resultado e: ', z)
+
+
---
+++ test_app.py
@@ -0,0 +1,9 @@
+import pytest
+from calculo import soma
+from calculo import result
+def test_app():
+    	assert soma(3) + 5 == 8
+def test_app():
+	assert result(4) * 4 == 16
+  
+
